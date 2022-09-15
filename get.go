372f3dde59196ad413f6484490f14786ce3ea947
李光春 <gc@dtapp.net>
2022-09-15 18:12:55 +0800

    - update

--- get.go
+++ get.go
@@ -1,9 +1,19 @@
 package pintoto
 
+import "go.dtapp.net/golog"
+
 func (c *Client) GetAppKey() string {
 	return c.config.appKey
 }
 
 func (c *Client) GetAppSecret() string {
 	return c.config.appSecret
+}
+
+func (c *Client) GetLogGorm() *golog.ApiClient {
+	return c.log.logGormClient
+}
+
+func (c *Client) GetLogMongo() *golog.ApiClient {
+	return c.log.logMongoClient
 }
